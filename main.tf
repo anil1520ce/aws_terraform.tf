847e3c50273f7ff957ac069b8ebaafa9d2ea50c2
anil1520ce <manilkumar.ce@gmail.com>
2023-03-31 17:32:24 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample-tf" {
 ami                     = var .ami_id
 instance_type           = var .instance_type
-key_name                = var .key_awsname
+key_name                = var .key_name
 vpc_security_group_ids  = var .sg_ids
 subnet_id               = var .subnet_id
 
